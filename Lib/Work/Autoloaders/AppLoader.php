62b6d2d2eaba985b8b6bf08a7f8f7579fa4f7a5c
Flávio <flvribeiro27@gmail.com>
2016-09-25 22:59:47 -0300

    Class to load other classes without namespaces

---
+++ Lib/Work/Autoloaders/AppLoader.php
@@ -0,0 +1,36 @@
+<?php
+namespace Work\Autoloaders;
+
+class AppLoader {
+	protected $folders = array();
+	
+	public function register() {
+		spl_autoload_register(array($this,'loadClass'));
+	}
+	
+	public function addDirectory($dir) {
+		$this->folders[] = $dir;
+	}
+	
+	public function loadClass($class) {
+		foreach($this->folders as $entry){
+			if(file_exists("{$entry}/{$class}.php")) {
+				require "{$entry}/{$class}.php";
+				return true;
+			}
+			else {
+				if(file_exists($entry)) {
+					foreach(new RecursiveIteratorIterator(new RecursiveDirectoryIterator($entry),RecursiveIteratorIterator::SELF_FIRST) as $try) {
+						if(is_dir($try)) {
+							if(file_exists("{$try}/{$class}.php") {
+								require "{$try}/{$class}.php";
+								return true;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+?>
