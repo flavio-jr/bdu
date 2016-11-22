ae9b52849b654d906e97a11def2cf0f297cbdc81
Flávio <flvribeiro27@gmail.com>
2016-11-22 13:42:54 -0300

    Update postController.js

--- App/Templates/js/postController.js
+++ App/Templates/js/postController.js
@@ -1,7 +1,6 @@
 var content = document.querySelectorAll(".prev");
 var posts = document.querySelectorAll(".prev h2 a");
 var i = 1;
-console.log("I'm here");
 while(i<=5) {
   console.log(posts[i-1].textContent);
   if(posts[i-1].textContent=='null') {
