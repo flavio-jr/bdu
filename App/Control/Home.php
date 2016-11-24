ee99db6acf95481c1dabb1d9a0f96469c7d48562
flavio-jr <flvrib@outlook.com>
2016-11-23 22:03:50 -0300

    Ajuste no atributo href

--- App/Control/Home.php
+++ App/Control/Home.php
@@ -55,50 +55,6 @@ class Home extends Page {
     echo $pag_post;
   }
 
-  /*
-   * homePage(): método utilizado para quando não há requisições
-  */
-  public static function homePage() {
-
-    /*Transaction::open('bdu');
-    Transaction::setLogger(new LoggerXML('homepage'));
-
-    $exibicao = new Criterion;
-
-    $regs = new Multi('Post');
-    $lastID = $regs->count();
-    $this->pages = (int) $regs->count()/5;
-
-    $exibicao->add(new Filter('id','<=',$lastID));
-    $exibicao->setProperty('limit',5);
-    $exibicao->setProperty('offset',$lastID-5);
-
-    $home = $regs->load($exibicao);
-
-    $j = 1;
-    $i = 4;
-    $pag_post = file_get_contents("App/Templates/home.html");
-    while($i>=0) {
-      //Implementar o design pattern MVC aqui
-      $post = new Post($home[$i]->id);
-      $tag = $post->getTag();
-      $date = self::formatDate($post->date);
-      $tittle = utf8_encode($post->tittle);
-      $cover = "'$post->img'";
-      $desc = utf8_encode($post->description);
-
-      $pag_post = str_replace("{{tag"."{$j}"."}}",utf8_encode($tag->tag_name),$pag_post);
-      $pag_post = str_replace("{{dt"."{$j}"."}}",$date,$pag_post);
-      $pag_post = str_replace("{{t"."{$j}"."}}",$tittle,$pag_post);
-      $pag_post = str_replace("{{i"."{$j}"."}}",$cover,$pag_post);
-      $pag_post = str_replace("{{d"."{$j}"."}}",$desc,$pag_post);
-
-      $j++;
-      $i--;
-    }
-
-    return $pag_post;*/
-  }
   private static function formatDate($date) {
     $foo = explode('-',$date);
     $newDate = $foo[2] . '|' . $foo[1] . '|' .substr($foo[0],2);
@@ -106,8 +62,8 @@ class Home extends Page {
 
   }
 
-  public function flip() {
-    self::$pag_atual = $_GET['num_page'];
+  public function flip($param) {
+    self::$pag_atual = $param['num_page'];
     Transaction::log("num".self::$pag_atual);
     echo $this->view();
   }
