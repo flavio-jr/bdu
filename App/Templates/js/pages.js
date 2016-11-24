bdaaf854276bca11280c2f3c178c78eaa97fb7d7
flavio-jr <flvrib@outlook.com>
2016-11-23 22:01:19 -0300

    Ajuste no atributo href

--- App/Templates/js/pages.js
+++ App/Templates/js/pages.js
@@ -5,5 +5,23 @@ while(control<=10) {
   if(pages[control-1].textContent=='{{pg'+control+'}}') {
     box[control-1].style.display = 'none';
   }
+  else {
+    if(document.URL) {
+      var urlCompleta = document.URL;
+      var get = urlCompleta.split("?")[1];
+      var object;
+      if(get) {
+        object = (get.indexOf("&")==-1) ? get : get.split("&")[0];
+      }
+      else {
+        object = "class=Home";
+      }
+      var pg = pages[control-1].textContent;
+      var href = pages[control-1].href;
+      var link = href.split("?")[1] + pg;
+
+      pages[control-1].href = "?" + object + link;
+    }
+  }
   control++;
 }
