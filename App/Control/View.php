65dbdfb4de167f329055f3daa17510ac1456fd13
flavio-jr <flvrib@outlook.com>
2016-12-02 21:45:44 -0300

    Organized folders

--- App/Control/View.php
+++ App/Control/View.php
@@ -78,7 +78,7 @@ use Work\Database\Multi;
    */
   public function formatDate($date) {
     $foo = explode('-',$date);
-    $newDate = $foo[2] . '/' . $foo[1] . '/' . substr($foo[1],2);
+    $newDate = $foo[2] . '/' . $foo[1] . '/' . substr($foo[0],2);
     return $newDate;
   }
 
